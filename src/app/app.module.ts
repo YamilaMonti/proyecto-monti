4b20b2bc762046878eb4fe60f4194eb922a0c4ba
YamilaMonti <yamila.monti217@gmail.com>
2020-06-02 00:35:57 -0300

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,17 +9,19 @@ import { InicioComponent } from './inicio/inicio.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
 import { NosotrosComponent } from './nosotros/nosotros.component';
 import { PedidosComponent } from './pedidos/pedidos.component';
+import { PieComponent } from './pie/pie.component';
 
 @NgModule({
   imports: [ 
     BrowserModule, 
     FormsModule,
     RouterModule.forRoot ([
+      { path: '', component: CabeceraComponent },
       { path: 'inicio', component: InicioComponent },
       { path: 'nosotros', component: NosotrosComponent },
       { path: 'pedidos', component: PedidosComponent}
       ]) ],
-  declarations: [ AppComponent, InicioComponent, CabeceraComponent, NosotrosComponent, PedidosComponent ],
+  declarations: [ AppComponent, InicioComponent, NosotrosComponent, PedidosComponent, CabeceraComponent, ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
