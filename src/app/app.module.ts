0df8fc6977ea251923a038c5e0f956290899b835
YamilaMonti <yamila.monti217@gmail.com>
2020-06-03 23:56:13 -0300

    4 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
 import { NosotrosComponent } from './nosotros/nosotros.component';
 import { PedidosComponent } from './pedidos/pedidos.component';
 import { ProductosComponent} from './productos/productos.component';
@@ -16,12 +17,12 @@ import { PieComponent } from './pie/pie.component';
     BrowserModule, 
     FormsModule,
     RouterModule.forRoot ([
-      { path: ' ', component: CabeceraComponent },
+      { path: '', component: InicioComponent },
       { path: 'nosotros', component: NosotrosComponent },
       { path: 'pedidos', component: PedidosComponent},
       { path: 'productos', component: ProductosComponent}
       ]) ],
-  declarations: [ AppComponent, CabeceraComponent,NosotrosComponent, PedidosComponent, ProductosComponent, PieComponent],
+  declarations: [ AppComponent, InicioComponent,CabeceraComponent,NosotrosComponent, PedidosComponent, ProductosComponent, PieComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/inicio/inicio.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { AppComponent } from '../app.component';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
+  styleUrls: ['./inicio.component.css']
+})
+export class InicioComponent implements OnInit {
+  ngOnInit() {
+  }
+
+}
